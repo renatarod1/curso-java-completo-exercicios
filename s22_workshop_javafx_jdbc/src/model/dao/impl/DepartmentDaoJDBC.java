562c00f8b0c88527f5a210712cb56746e0a19af0
Renata Rodrigues <ticare1@hotmail.com>
2020-06-24 19:49:24 -0300

    Secao 23 - workshop-javafx-jdbc - Remove department

--- s22_workshop_javafx_jdbc/src/model/dao/impl/DepartmentDaoJDBC.java
+++ s22_workshop_javafx_jdbc/src/model/dao/impl/DepartmentDaoJDBC.java
@@ -10,6 +10,7 @@ import java.util.List;
 
 import db.DB;
 import db.DbException;
+import db.DbIntegrityException;
 import model.dao.DepartmentDao;
 import model.entities.Department;
 
@@ -83,7 +84,7 @@ public class DepartmentDaoJDBC implements DepartmentDao {
 				throw new DbException("Id not found!");
 			}
 		} catch (SQLException e) {
-			throw new DbException(e.getMessage());
+			throw new DbIntegrityException(e.getMessage());
 		} finally {
 			DB.closeStatement(st);
 		}
